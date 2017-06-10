745b3cdcb1e1b9822b07ddd18669969a13ba4e83
Mark Piper <mark.piper@wds.co>
2017-06-10 13:57:22 +0100

    Basic template of a Kotlin/Spark/KotlinTest project

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,49 @@
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import spark.ModelAndView
+import spark.Spark.*
+import spark.TemplateEngine
+import spark.template.velocity.VelocityTemplateEngine
+
+val logger: Logger = LoggerFactory.getLogger("main")
+
+fun main(args: Array<String>) {
+
+    val application = Application(8080)
+    application.ignite()
+}
+
+
+class Application(val port: Int = 8080) {
+
+    private var ignited = false
+
+    val renderEngine: TemplateEngine
+        get() = VelocityTemplateEngine()
+
+    fun ignite() {
+        port(8080)
+
+        get("/") { _, _ ->
+            render("hello.vm")
+         }
+
+        exception(Exception::class.java) { exc, _, res ->
+            logger.error(exc.message)
+            res.body(exc.message)
+        }
+
+        awaitInitialization()
+        ignited = true
+    }
+
+    fun extinguish() {
+        if(ignited) stop()
+    }
+
+    fun render(templateName: String,
+               model: Any = emptyMap<String, String>()) = renderEngine.render(ModelAndView(model, "templates/$templateName"))
+
+}
+
